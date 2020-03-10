48e15d86f67906248012946c6b7a7022d9c523bd
ihatelag01 <61805739+ihatelag01@users.noreply.github.com>
2020-03-10 19:20:00 +0200

    Add files via upload

---
+++ Farmacie/Medicamente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacie
+{
+    class Medicamente
+    {
+        string nume;
+        double pret;
+
+        public  Medicamente()
+        {
+            pret = 0;
+            string nume =string.Empty;
+            
+
+      
+        }
+        
+          
+         public   Medicamente(string _nume,double _pret)
+        {
+            nume = _nume;
+            pret = _pret;
+        }
+
+        public string Afisare()
+        {
+            if (nume.Length == 0)
+
+                return "NEDEFINIT";
+            else
+                return string.Format("{0} {1} RON\n", nume, pret);
+        }
+       
+    }
+        
+    }
+
---
+++ Farmacie/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farmacie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Medicamente m1 = new Medicamente("NUROFEN", 20);
+            string s1=m1.Afisare();
+            Console.WriteLine(s1);
+        
+        }
+    }
+}
