58c083906aee42e3bf1ebb2e3cc2ea10618dc767
ihatelag01 <61805739+ihatelag01@users.noreply.github.com>
2020-04-03 12:13:14 +0300

    Add files via upload

--- Farmacie/Medicamente.cs
+++ Farmacie/Medicamente.cs
@@ -11,22 +11,42 @@ namespace Farmacie
          public string nume { get; set; }
 
         public double pret { get; set; }
-        public string infoComplet { get { return nume + " " + Convert.ToString(pret); } }
+        public string infoComplet { get { return nume.ToUpper() + " " + Convert.ToString(pret) + " " + "RON" + "," + Convert.ToString(cantitate) + " " + "buc."; } }
+        public int cantitate { get; set; }
+        public Tip TIP { get; set; }
+        public Prescriptie PRES { get; set; }
+        public enum Tip
+        {   
+            NEDEFINIT=0,
+            COMPRIMATE=1,
+            SIROP=2,
+            UNGUENT=3,
+        }
+        public enum Prescriptie
+        {
+            DA=1,
+            NU=0,
+        }
 
         public  Medicamente()
         {
             pret = 0;
             string nume =string.Empty;
+            cantitate = 0;
+            TIP = 0;
+            PRES = 0;
             
 
       
         }
         
           
-         public   Medicamente(string _nume,double _pret)
+         public   Medicamente(string _nume,double _pret,int _tip,int _pres)
         {
             nume = _nume;
             pret = _pret;
+            TIP = (Tip)_tip;
+            PRES = (Prescriptie)_pres;
         }
 
         public string Afisare()
@@ -35,7 +55,7 @@ namespace Farmacie
 
                 return "NEDEFINIT";
             else
-                return string.Format("{0} {1} RON\n", nume, pret);
+                return string.Format("{0} costa {1} RON,tip {2},necesita prescriptie {3}.\n", nume.ToUpper(), pret,TIP,PRES);
         }
         public Medicamente(string info)
         {
@@ -62,16 +82,22 @@ namespace Farmacie
         public int Compara(Medicamente ob)
         {
             if (pret > ob.pret) 
-                
+            {
+                Console.WriteLine("Pret mai mare!");
                 return 1;
-            
+            }
+
             if (pret < ob.pret)
-            
-                
+            {
+                Console.WriteLine("Pret mai mic!");
                 return -1;
-            
+            }
+
             else
+            {
+                Console.WriteLine("Preturi egale!");
                 return 0;
+            }
                 
              
         }
--- Farmacie/Program.cs
+++ Farmacie/Program.cs
@@ -6,19 +6,139 @@ namespace Farmacie
     {
         static void Main(string[] args)
         {
+            Medicamente[] med = new Medicamente[100];
+            int nrMed = 0;
 
-            Medicamente m1 = new Medicamente("Nurofen", 20);// Constructor paramatrii
-            string s1=m1.Afisare();
-            Console.WriteLine(s1);
-            Medicamente m2 = new Medicamente("Paracetamol,11");// Constructor sir de caractere
-            string s2 = m2.Afisare();
-            Console.WriteLine(s2);
-            Console.WriteLine(m2.Compara(m1));
-            Console.WriteLine("Introduceti denumirea si pretul: ");
-            Medicamente m3 = new Medicamente(Console.ReadLine(),Convert.ToDouble(Console.ReadLine()));//Citire date de la tastatura
-            Console.WriteLine(m3.infoComplet);
-            
-            
+
+            while (true)
+            {
+                Console.WriteLine("Alegeti o optiune:");
+                Console.WriteLine("A)Adaugare medicament.");
+                Console.WriteLine("B)Afisare lista medicamente.");
+                Console.WriteLine("F)Cautare medicament dupa nume si afisare cantitate.");
+                Console.WriteLine("G)Cautare medicamente dupa pret si afisare medicamente cu pretul respectiv.");
+                Console.WriteLine("I)Info autor.");
+                Console.WriteLine("X)Iesire");
+                string opt = Console.ReadLine();
+                Console.Clear();
+                switch (opt.ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine("Introduceti denumirea: ");
+
+                        string s1 = Console.ReadLine();
+                        if (s1 == string.Empty)
+                        {
+                            Console.WriteLine("Denumire incorecta,reintroduceti: ");
+                            s1 = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Introduceti pretul");
+                        double p;
+
+                        p = Convert.ToDouble(Console.ReadLine());
+                        if (p < 1)
+                        {
+                            Console.WriteLine("Pret invalid,reintroduceti:");
+                            p = Convert.ToDouble(Console.ReadLine());
+                        }
+                        int type;
+                        Console.WriteLine("Introduceti tipul:");
+                        Console.WriteLine("1)Comprimat.");
+                        Console.WriteLine("2)Sirop.");
+                        Console.WriteLine("3)Unguent.");
+                        type = Convert.ToInt32(Console.ReadLine());
+                        if (type < 0 || type > 3)
+                        {
+                            Console.WriteLine("Tip invalid,reintroduceti");
+                            type = Convert.ToInt32(Console.ReadLine());
+                        }
+                        int pres;
+                        Console.WriteLine("Necesita prescriptie?");
+                        Console.WriteLine("0)Nu");
+                        Console.WriteLine("1)Da");
+                        pres = Convert.ToInt32(Console.ReadLine());
+                        if (pres != 0 && pres != 1)
+                        {
+                            Console.WriteLine("Optiune invalida,reintroduceti:");
+                            pres = Convert.ToInt32(Console.ReadLine());
+                        }
+                        nrMed++;
+
+
+                        med[nrMed] = new Medicamente(s1, p, type, pres);
+
+                        break;
+                    case "B":
+                        for (int i = 1; i <= nrMed; i++)
+                        {
+                            Console.WriteLine(i + "." + med[i].Afisare());
+                        }
+                        break;
+                    case "F":
+                        Console.WriteLine("Introduceti denumirea medicamentului cautat:");
+                        string s2 = Console.ReadLine();
+                        for (int i = 1; i <= nrMed; i++)
+                        {
+                            if (s2 == med[i].nume)
+                            {
+                                Console.WriteLine("Medicament gasit,stoc" + " " + med[i].cantitate + " " + "bucati");
+                                break;
+                            }
+                            else
+                            {
+
+                                Console.WriteLine("Medicamentul introdus nu a fost gasit.");
+                                continue;
+
+
+
+                            }
+
+
+
+                        }
+                        break;
+                    case "I":
+                        Console.WriteLine("Vizitiu Alexandru 3123b");
+                        break;
+                    case "X":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Optiune invalida,incercati din nou!");
+                        break;
+                    case "G":
+                        Console.WriteLine("Introduceti pretul dorit: ");
+                        double y = Convert.ToDouble(Console.ReadLine());
+                        if (y < 1)
+                        {
+                            Console.WriteLine("Pret invalid,reintroduceti");
+                            y = Convert.ToDouble(Console.ReadLine());
+                        }
+                        Console.WriteLine("Urmatoarele medicamente corespund pretului introdus:");
+                        for (int i = 1; i <= nrMed; i++)
+                        {
+                            if (med[i].pret == y)
+                            {
+                                Console.WriteLine(med[i].nume.ToUpper());
+                                continue;
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Niciun medicament cu pretul introdus nu a fost gasit.");
+                                continue;
+                            }
+
+                        }
+                        break;
+
+                }
+
+            }
+
+
         }
     }
 }
